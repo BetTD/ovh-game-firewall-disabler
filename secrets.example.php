7a1766b8b634f8d8cda84aa21576025c87798b47
BetTD <between13131@gmail.com>
2023-04-05 07:16:55 +0200

    Add comments to secrets example file

--- secrets.example.php
+++ secrets.example.php
@@ -1,6 +1,34 @@
 <?php
+/*
+ * OBTAINING YOUR API CREDENTIALS
+ *
+ * Visit https://github.com/ovh/php-ovh#supported-endpoints to learn how to obtain the credentials!
+ * Here's a quick summary:
+ *
+ * OVHcloud Europe ->
+ *      Endpoint: ovh-eu
+ *      Create API keys: https://eu.api.ovh.com/createToken/
+ * OVHcloud US ->
+ *      Endpoint: ovh-us
+ *      Create API keys: https://api.us.ovhcloud.com/createToken/
+ * OVHcloud NA/CA ->
+ *      Endpoint: ovh-ca
+ *      Create API keys: https://ca.api.ovh.com/createToken/
+ * So you Start Europe ->
+ *      Endpoint: soyoustart-eu
+ *      Create API keys: https://eu.api.soyoustart.com/createToken/
+ * So you Start NA/CA ->
+ *      Endpoint: soyoustart-ca
+ *      Create API keys: https://ca.api.soyoustart.com/createToken/
+ * Kimsufi Europe ->
+ *      Endpoint: kimsufi-eu
+ *      Create API keys: https://eu.api.kimsufi.com/createToken/
+ * Kimsufi NA/CA ->
+ *      Endpoint: kimsufi-ca
+ *      Create API keys: https://ca.api.kimsufi.com/createToken/
+ */
+
+CONST ENDPOINT = "CHANGEME";
 const APP_KEY = "";
 const APP_SECRET = "";
 const CONSUMER_KEY = "";
-# see: https://github.com/ovh/php-ovh#supported-endpoints
-CONST ENDPOINT = "ovh-ca";
