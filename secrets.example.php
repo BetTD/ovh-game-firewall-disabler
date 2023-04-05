7cb28389067e35c4ef851d594fd942bdca7e3659
BetTD <between13131@gmail.com>
2023-04-05 07:39:25 +0200

    Minor changes to comments

--- secrets.example.php
+++ secrets.example.php
@@ -7,25 +7,25 @@
  *
  * OVHcloud Europe ->
  *      Endpoint: ovh-eu
- *      Create API keys: https://eu.api.ovh.com/createToken/
+ *      Create API credentials: https://eu.api.ovh.com/createToken/
  * OVHcloud US ->
  *      Endpoint: ovh-us
- *      Create API keys: https://api.us.ovhcloud.com/createToken/
+ *      Create API credentials: https://api.us.ovhcloud.com/createToken/
  * OVHcloud NA/CA ->
  *      Endpoint: ovh-ca
- *      Create API keys: https://ca.api.ovh.com/createToken/
+ *      Create API credentials: https://ca.api.ovh.com/createToken/
  * So you Start Europe ->
  *      Endpoint: soyoustart-eu
- *      Create API keys: https://eu.api.soyoustart.com/createToken/
+ *      Create API credentials: https://eu.api.soyoustart.com/createToken/
  * So you Start NA/CA ->
  *      Endpoint: soyoustart-ca
- *      Create API keys: https://ca.api.soyoustart.com/createToken/
+ *      Create API credentials: https://ca.api.soyoustart.com/createToken/
  * Kimsufi Europe ->
  *      Endpoint: kimsufi-eu
- *      Create API keys: https://eu.api.kimsufi.com/createToken/
+ *      Create API credentials: https://eu.api.kimsufi.com/createToken/
  * Kimsufi NA/CA ->
  *      Endpoint: kimsufi-ca
- *      Create API keys: https://ca.api.kimsufi.com/createToken/
+ *      Create API credentials: https://ca.api.kimsufi.com/createToken/
  */
 
 CONST ENDPOINT = "CHANGEME";
